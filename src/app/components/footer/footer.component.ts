4d7349ef9042d8600b2bb3fd15714a2b52a61108
Askaxio <jorgesanchezisn@gmail.com>
2024-05-23 23:58:38 -0600

    jijijij

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  isFooterVisible: boolean = true;
 
+  toggleFooter(): void {
+    this.isFooterVisible = !this.isFooterVisible;
+  }
 }
